c744d86b216277ba49b605b5625012e207b5e5fb
ScrappyioV2 <jaydevbhandva45@gmail.com>
2026-01-13 12:29:23 +0530

    manthan - generated link progress bar solve

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -7,3 +7,7 @@ export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 );
+
+
+
+
--- lib/utils.ts
+++ lib/utils.ts
@@ -47,3 +47,9 @@ export const determineCategory = (monthlyUnit: number | null): {
 export const getTableName = (sellerId: number, category: string): string => {
   return `usa_seller_${sellerId}_${category.replace('-', '_')}`;
 };
+
+
+
+
+
+
