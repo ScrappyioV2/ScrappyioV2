2c5ea2c09d26f489f5a424f266041314cb234094
MR J <jaydevbhandva45@gmail.com>
2026-05-13 16:15:26 +0530

    Revert "fix: active row highlight + stable sort in flipkart brand checking (no more shuffling)"
    This reverts commit 6db19d30db45f5d15456adf3d5d6ab118f654905.

--- supabase/docker/volumes/snippets/Untitled query 797.sql
+++ supabase/docker/volumes/snippets/Untitled query 797.sql
@@ -1,9 +1,1 @@
--- Purchase copies
-UPDATE india_purchase_copies 
-SET buying_quantity = 2, buying_quantities = '{"VV": 2}' 
-WHERE id = 1002;
-
--- Admin validation
-UPDATE india_admin_validation 
-SET buying_quantity = 2 
-WHERE id = '636f3a38-9092-4e74-b536-d87ff4679e79';
+SELECT seller_id AS seller, total FROM flipkart_brand_check_progress ORDER BY seller_id;
